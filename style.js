bb8ee891292648bc7d35b1c8799af7ec0d659c83
JairoD23 <89793964+JairoD23@users.noreply.github.com>
2024-02-20 07:22:23 -0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,9 @@
+const img = document.querySelector('header img');
+
+// Obtener el ancho y alto de la imagen
+const width = img.width;
+const height = img.height;
+
+// Mostrar el tamaño de la imagen en la consola
+console.log(`Ancho: ${width}px`);
+console.log(`Alto: ${height}px`);
